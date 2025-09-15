8f85aad49af5ffaa0eff65989d503edb6e91569d
lv <elvisflorez58@gmail.com>
2025-09-15 08:54:05 -0500

    Primer commit con los archivos correctos

--- src/pages/api/love.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-const msg = ['hello kat', 'kat love', 122];
-export const GET: APIRoute = ({ params, request }) => {
-  return new Response(JSON.stringify({ mensaje: msg[0] }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" }
-  });
-};
--- src/utils/db.ts
+++
@@ -1,7 +0,0 @@
-
-
- 
-// export async function getStaticPaths() {
-//   const pets = await fetch('https://api.example.com/pets').then(r => r.json());
-//   return pets.map(p => ({ params: { id: p.id }}));
-// }
